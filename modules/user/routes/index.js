08814f604843193e9b8e47c4d6225af394341d9e
leotalorac <leotalorac@unal.edu.co>
2020-05-04 09:19:05 -0500

    add debug

--- modules/user/routes/index.js
+++ modules/user/routes/index.js
@@ -5,7 +5,6 @@ var router = require('express').Router();
 // get the model
 const UserController = require('../controllers/index')
 
-
 router.post('/sign-up', UserController.register);
 router.post('/login', UserController.login);
 router.post('/check',UserController.checkToken)
