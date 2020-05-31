c285145e78db3a0a4c754c1ffe2a41e40c9d21c9
leotalorac <leotalorac@unal.edu.co>
2020-05-31 17:55:17 -0500

    to deploy

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const config = require('../config/config.json')["local"];
+const config = require('../config/config.json')["development"];
 let newconfig = {
     ...config,
     define: {
